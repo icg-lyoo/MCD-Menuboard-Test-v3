a8a9bd9ed9cd3ad4bf0c65d81aaf391bbd9659d2
Claudio Nuñez Jr <cnunez@icginfo.com>
2013-04-18 12:13:41 -0700

    Enabled daypart auto switching.

--- js/daypart-controller-MCDV3.js
+++ js/daypart-controller-MCDV3.js
@@ -7,7 +7,7 @@ var daypartController = {
     },
     run: function() {
         daypartController.switchDaypartOnKeyPress();
-        // daypartController.switchDaypartAuto();
+        daypartController.switchDaypartAuto();
     },
     // switch daypart on the fly (on keypress), if the current board is not showing the daypart pressed
     switchDaypartOnKeyPress: function() {
